15d950a0b617fed0950e73889fc02caa495a42b1
Nicholas Provencal <31230393+ViridianImpact@users.noreply.github.com>
2020-05-01 08:26:04 -0700

    pygame initialization primary player movement
    added player class
    added movement function INCOMPLETE

--- ps1.py
+++ ps1.py
@@ -15,8 +15,33 @@ pistol mods available:
     life steal
     reduced mana cost"""
 import random
+import pygame
+pygame.init()
 from colorama import Fore, Style
 
+class Player:
+    def __init__(self):
+        self.x = 100
+        self.y = 100
+        self.vel = 5
+        self.width = 40
+        self.height = 60
+
+    def move_player(self, mouse_position):
+        x_final = mouse_position[0]
+        y_final = mouse_position[1]
+        distance = ((self.x - x_final)** 2 + (self.y - y_final)** 2)** 0.5
+        walk = True
+
+        while walk:
+            self.x = self.x + (self.vel / distance)*(x_final - self.x)
+            self.y = self.y + (self.vel / distance)*(y_final - self.y)
+
+            pygame.draw.rect(active_window, (255, 0, 0), (player.x, player.y, player.width, player.height))
+
+            if self.x < (x_final + 1) and self.x > (x_final - 1):
+                if self.y > (y_final - 1) and self.y < (y_final + 1):
+                    walk = False
 
 class CharacterBase:
     """define all parameters"""
@@ -45,7 +70,7 @@ class MapTile:
     def print_types(self):
         """print the types of each item on the map"""
         for i in range(len(live_map.ItemsList)):
-            print(live_map.ItemsList[i].type)
+            print(live_map.ItemsList[i].type)        
 
 class Item:
     """define all parameters"""
@@ -140,10 +165,33 @@ RAND = 0
 live_map = 0
 GENERATORS = ["Weapon", "Support", "Bridge", "PowerCore", "LifeSupport", "Thruster", "HullMaterial",
               "Accessory", "Gem"]
+player = Player()
 
 print(Fore.WHITE + Style.BRIGHT + "RAND is " + str(RAND) + "\n")
 
 print(Fore.WHITE + Style.BRIGHT + "\n")
 live_map = MapTile()
+
+active_window = pygame.display.set_mode((1840, 990))
+pygame.display.set_caption("My Game")
+
+
+run = True
+
+while run:
+    pygame.time.delay(10)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_position = pygame.mouse.get_pos()
+            player.move_player(mouse_position)
+
+    pygame.draw.rect(active_window, (255, 0, 0), (player.x, player.y, player.width, player.height))
+
+    pygame.display.update()
+
+pygame.quit()
 
 print(Fore.WHITE + Style.BRIGHT + "End of runtime successful")
